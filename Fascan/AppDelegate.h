104646acda2b3f7c1276d6feeabbbfa697ccf93e
policp <chenpeng@chenpengdeMac-mini.local>
2015-07-15 10:46:41 +0800

    Initial Commit

---
+++ Fascan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fascan
+//
+//  Created by chenpeng on 15/7/15.
+//  Copyright (c) 2015年 chenpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
